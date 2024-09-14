a35f5c1a3840c043f0cabec4b429b6cfb37589d0
Raul Barreiro <Raul95ml@hotmail.com>
2024-09-14 11:08:46 -0500

    configurar gitignore y añadir .env(se cambiará de enfoque en siguientes commits, por el momenmto para el front se mantiene el .env)

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL //http://localhost:3001/producto/41;
 console.log(process.env);
 console.log(API_URL);
 
